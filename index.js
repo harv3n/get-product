8c0dff135901a48ec841596843271dc78aed14e0
Harley Venturini <venturiniharley@gmail.com>
2019-06-27 01:06:02 -0300

    atualizacao de comentarios

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * Modulo de scraping de dados de produtos do site 'americadas.com'
+ * Desenvolvido em proposta do desafio técnico da B2W
+ * 
+ * ./lib/getProduct -> Executa a ação principal do modulo
+ * ./lib/handle -> Criado com o intuito de efetuar o tratamento dos dados existentes e de quaisquer dados posteriores
+ */
+
 exports = module.exports = require('./lib/getProduct');
 
 exports.version = require('./package.json').version;
